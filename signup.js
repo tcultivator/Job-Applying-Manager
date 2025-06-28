32af1b8fec236c3f09370cc8f3ab54bbb7458ccc
tcultivator <whypanahon06@gmail.com>
2025-06-29 05:57:24 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,34 +1,43 @@
 const username = document.getElementById('username')
 const password = document.getElementById('password')
+const confirmpassword = document.getElementById('confirmpassword')
 
 
 document.getElementById('signupForm').addEventListener('submit', async (e) => {
     e.preventDefault()
-    const signup = await fetch('https://job-applying-manager.onrender.com/signup', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username: username.value, password: password.value })
-    })
-    const data = await signup.json()
-    if (signup.ok) {
-        console.log(data.message)
-        document.getElementById('notifbody').style = `display:flex; background-color: rgb(66, 226, 146);`
-        document.getElementById('message').textContent = data.message
-        setTimeout(() => {
-            document.getElementById('notifbody').style = `display:none`
-            window.location.replace('login.html')
-        }, 1500);
-
-    }
-    else {
-        console.log(data.message)
+    if (password.value != confirmpassword.value) {
         document.getElementById('notifbody').style = `display:flex;background-color: rgb(219, 71, 71);`
-        document.getElementById('message').textContent = data.message
+        document.getElementById('message').textContent = 'Password not match!'
         setTimeout(() => {
             document.getElementById('notifbody').style = `display:none`
         }, 1500);
+    } else {
+        const signup = await fetch('https://job-applying-manager.onrender.com/signup', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: username.value, password: password.value })
+        })
+        const data = await signup.json()
+        if (signup.ok) {
+            console.log(data.message)
+            document.getElementById('notifbody').style = `display:flex; background-color: rgb(66, 226, 146);`
+            document.getElementById('message').textContent = data.message
+            setTimeout(() => {
+                document.getElementById('notifbody').style = `display:none`
+                window.location.replace('login.html')
+            }, 1500);
+
+        }
+        else {
+            console.log(data.message)
+            document.getElementById('notifbody').style = `display:flex;background-color: rgb(219, 71, 71);`
+            document.getElementById('message').textContent = data.message
+            setTimeout(() => {
+                document.getElementById('notifbody').style = `display:none`
+            }, 1500);
+        }
     }
 })
